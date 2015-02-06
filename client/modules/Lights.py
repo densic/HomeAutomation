1f6f73724d71921cf05a695a150e21ef73b8ded4
Qi <pi@rpi.(none)>
2015-02-06 21:48:58 +0000

    first commit. Clone Jasper project and check it in to new repo

---
+++ client/modules/Lights.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8-*-
+import random
+import re
+
+WORDS = ["LIGHTS", "On"]
+
+
+def handle(text, mic, profile):
+    """
+        Responds to user-input, typically speech text, by relaying the
+        meaning of life.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user (e.g., phone
+                   number)
+    """
+    messages = ["Lights already on, you idiot.",
+                "Turning lights on, xiaohan I love you"]
+
+    message = random.choice(messages)
+
+    mic.say(message)
+
+
+def isValid(text):
+    """
+        Returns True if the input is related to the meaning of life.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+    """
+    return bool(re.search(r'\blights on\b', text, re.IGNORECASE))
